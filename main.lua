42517399ffcef383fa0ff9adb223d345bfc8bbfa
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-08-04 08:46:06 -0500

    Did more

--- main.lua
+++ main.lua
@@ -1,35 +1,83 @@
 pancake =  require "pancake"
 
-
-local score = 0
-local current_binary = 0
+local screen
 local on
 local off
+
+local highscore = 0
+local score = 0
+local current_binary = 0
 local active = false
 local game_over = false
+local before_spawn = true
 
-local spawn_interval = 5
+local base_spawn_interval = 10
+local base_speed = 20
+
+local speed = base_speed
+local spawn_interval = base_spawn_interval
 local spawn_timer = 0
 
 local numbers = {}
+local buttons = {}
 
-local function addBitButton(x, y, position)
-	return pancake.addButton({ name = tostring(position), x = pancake.window.x + x * pancake.window.pixelSize, y = y * pancake.window.pixelSize, image = off, imageClicked = on, width = 4, height = 4, scale = pancake.window.pixelSize, func = function() toggleBit(position) end })
+local function addBitButton(x, y, position, key)
+	local button = pancake.addButton({ name = tostring(position), x = pancake.window.x + x * pancake.window.pixelSize, y = y * pancake.window.pixelSize, image = off, imageClicked = on, width = 4, height = 4, scale = pancake.window.pixelSize, key = tostring(key) })
+	button.toggled = false
+	button.func = function()
+		toggleBit(position)
+		button.toggled = not button.toggled
+	end
+	buttons[position] = button
+	return button
 end
 
-local function drawCurrentBinary(x, y)
-	pancake.print(tostring(current_binary), pancake.window.x + x * pancake.window.pixelSize, y * pancake.window.pixelSize, pancake.window.pixelSize)
+local function randomColor()
+	return { r = love.math.random(255, 127) / 255, g = love.math.random(255, 127) / 255, b = love.math.random(255, 127) / 255, a = 1 }
+end
+
+local function percentile()
+	return love.math.random(100, 1) / 100
+end
+
+local function drawText(text, x, y)
+	pancake.print(text, pancake.window.x + x * pancake.window.pixelSize, y * pancake.window.pixelSize, pancake.window.pixelSize)
 end
 
 local function spawnNumber()
-	local number = love.math.random(255, 0)
-	local width = pancake.getStringWidth(tostring(number))
-	local spawn_x = love.math.random(pancake.window.x + (3 + width) * pancake.window.pixelSize, pancake.window.x + (60 - width) * pancake.window.pixelSize)
-	local spawn_y = pancake.window.height + 3 * pancake.window.pixelSize
+	local number = love.math.random(math.min(255, 15 + math.pow(2, score)), 0)
+	local num_str = tostring(number)
+	if score >= 20 then
+		num_str = string.format("0x%02X", number)
+	end
 
-	local spawn = pancake.addObject({ x = spawn_x, y = spawn_y, width = width, height = 5, colliding = true, name = tostring(number) })
+	local width = pancake.getStringWidth(num_str)
+	local spawn_x = pancake.window.x + love.math.random(4, 61 - width) * pancake.window.pixelSize
+	local spawn_y = pancake.window.height + -10 * pancake.window.pixelSize
+
+	local spawn = pancake.addObject({ x = spawn_x, y = spawn_y, width = width, height = 5, colliding = true, name = num_str })
+	spawn.num_type = num_type
+	spawn.color = randomColor()
+	spawn.number = number
 	numbers[#numbers + 1] = spawn
-	pancake.applyPhysics(spawn)
+end
+
+local function drawLegend(x, y)
+	local scale = pancake.window.pixelSize / 8
+	pancake.print("Keybinds:", pancake.window.x + x * pancake.window.pixelSize, pancake.window.y + y * pancake.window.pixelSize, scale)
+	pancake.print("Press \"Escape\" to quit the game", pancake.window.x + x * pancake.window.pixelSize, pancake.window.y + (y + 1.1) * pancake.window.pixelSize, scale)
+	pancake.print("Press \"1\" to \"8\" to set bits, left to right", pancake.window.x + x * pancake.window.pixelSize, pancake.window.y + (y + 2.2) * pancake.window.pixelSize, scale)
+	pancake.print("The Red Bit Boxes are also clickable buttons!", pancake.window.x + x * pancake.window.pixelSize, pancake.window.y + (y + 3.2) * pancake.window.pixelSize, scale)
+end
+
+local function drawBit(x, y, position)
+	local b = bit.band(bit.rshift(current_binary, position), 1)
+	if b == 1 then
+		drawText(tostring(b), x + .5, y)
+	else
+		drawText(tostring(b), x, y)
+	end
+	drawText("!", x + 1.5, y + 6)
 end
 
 function toggleBit(position)
@@ -37,99 +85,130 @@ function toggleBit(position)
 end
 
 function love.load()
-	pancake.init({ window = { pixelSize = love.graphics.getHeight()/64 }, physics = { gravityY = 10 } }) --Initiating pancake and setting pixelSize, so that the pancake display will be the height of the window! pixelSize is how many pixels every pancake pixel should take
+	pancake.init({ window = { pixelSize = love.graphics.getHeight()/64 }, background = { r = 0, g = 0, b = 0, a = 1 } })
 	
-	pancake.addImage("screen", "images")
+	screen = pancake.addImage("screen", "images")
 	on = pancake.addImage("on", "images")
 	off = pancake.addImage("off", "images")
-	pancake.addObject({ x = 0, y = 0, width = 64, height = 64, image = "screen", colliding = false })
-	pancake.addObject({ x = pancake.window.height, y = pancake.window.height + 64 * pancake.window.pixelSize, width = 64, height = 12, name = "hitbox", colliding = false })
+
+	if love.filesystem.getInfo("highscore.sav") then
+        highscore = pancake.load("highscore.sav", "table").value
+    end
 end
 
-function pancake.onCollision() --This function will be called whenever a physic object collides with a colliding object!
-	--Insert your amazing code here!
-end
-
-function pancake.onLoad() -- This function will be called when pancake start up is done (after the animation)
+function pancake.onLoad()
 	active = true
 
-	addBitButton(5, 56, 7)
-	addBitButton(12, 56, 6)
-	addBitButton(19, 56, 5)
-	addBitButton(26, 56, 4)
+	addBitButton(5, 49, 7, 1)
+	addBitButton(12, 49, 6, 2)
+	addBitButton(19, 49, 5, 3)
+	addBitButton(26, 49, 4, 4)
 
-	addBitButton(34, 56, 3)
-	addBitButton(41, 56, 2)
-	addBitButton(48.01, 56, 1) -- weirdly needed
-	addBitButton(55, 56, 0)
-end
-
-function pancake.onOverlap(object1, object2, dt) -- This function will be called every time object "collides" with a non colliding object! Parameters: object1, object2 - objects of collision, dt - time of collision
-	--Insert your amazing code here!
-
-	if (object1.name == "hitbox" and tonumber(object2.name)) or (object2.name == "hitbox" and tonumber(object1.name)) then
-		game_over = true
-	end
+	addBitButton(34, 49, 3, 5)
+	addBitButton(41, 49, 2, 6)
+	addBitButton(48.01, 49, 1, 7) -- weirdly needed
+	addBitButton(55, 49, 0, 8)
 end
 
 function love.draw()
-	pancake.draw() --Sets the canvas right! If pancake.autoDraw is set to true (which is its default state) the canvas will be automatically drawn on the window x and y
-
-	pancake.print("Score: " .. tostring(score))
-	pancake.print("Number of Numbers: " .. tostring(#numbers), 0, 10)
-	pancake.print("Active: " .. tostring(active), 0, 20)
-	pancake.print("Game Over: " .. tostring(game_over), 0, 30)
-	pancake.print("Spawn Interval: " .. tostring(spawn_interval), 0, 40)
-	pancake.print("Gravity: " .. tostring(pancake.physics.gravityY), 0, 50)
+	pancake.draw()
 
 	if not active then return end
 
+	drawLegend(64.25, 28)
+
+	if before_spawn then
+		drawText("Match Binary", 32 - pancake.getStringWidth("Match Binary") / 2, 14)
+		drawText("to Numbers", 32 - pancake.getStringWidth("to Numbers") / 2, 20)
+		drawText("to Score!", 32 - pancake.getStringWidth("to Score!") / 2, 26)
+
+		drawBit(5, 39, 7)
+		drawBit(12, 39, 6)
+		drawBit(19, 39, 5)
+		drawBit(26, 39, 4)
+
+		drawBit(34, 39, 3)
+		drawBit(41, 39, 2)
+		drawBit(48, 39, 1)
+		drawBit(55, 39, 0)
+	end
+
 	if not game_over then
-		if current_binary / 100 > 1 then
-			drawCurrentBinary(25, 47)
-		elseif current_binary / 10 > 1 then
-			drawCurrentBinary(28, 47)
-		else
-			drawCurrentBinary(30, 47)
+		local binary_str = tostring(current_binary)
+		if score >= 20 then
+			binary_str = string.format("0x%02X", current_binary)
 		end
+		drawText(binary_str, 6, 56)
+
+		drawText(tostring(score), 59 - pancake.getStringWidth(tostring(score)), 56)
 
 		for _, number in pairs(numbers) do
+			love.graphics.setColor(number.color.r, number.color.g, number.color.b, number.color.a)
 			pancake.print(number.name, number.x, number.y, pancake.window.pixelSize)
+			love.graphics.setColor(1, 1, 1, 1)
+		end
+
+		love.graphics.draw(screen, pancake.window.x, pancake.window.y, 0, pancake.window.pixelSize)
+
+		for _, button in pairs(buttons) do
+			local img = button.image
+			if button.toggled then
+				img = button.imageClicked
+			end
+			love.graphics.draw(img, button.x, button.y, 0, button.scale)
 		end
 	else
+		if score > highscore then
+			highscore = score
+			pancake.save({value = highscore}, "highscore.sav")
+		end
+
 		local score_str = "Score: " .. tostring(score)
-		local width = pancake.getStringWidth(score_str)
-		pancake.print("Game Over!", pancake.window.x + (32 - (pancake.getStringWidth("Game Over!") / 2)) * pancake.window.pixelSize, pancake.window.y + 20 * pancake.window.pixelSize, pancake.window.pixelSize)
-		pancake.print(score_str, pancake.window.x + (32 - (width / 2)) * pancake.window.pixelSize, pancake.window.y + 30 * pancake.window.pixelSize, pancake.window.pixelSize)
+		local highscore_str = "Highscore: " .. tostring(highscore)
+		drawText("Game Over!", 32 - pancake.getStringWidth("Game Over!") / 2, 10)
+		drawText(highscore_str, 32 - pancake.getStringWidth(highscore_str) / 2, 20)
+		drawText(score_str, 32 - pancake.getStringWidth(score_str) / 2, 30)
 	end
 end
 
 function love.update(dt)
 	if game_over then return end
-	pancake.update(dt) --Passing time between frames to pancake!
+	pancake.update(dt)
 
 	if not active then return end
 
 	spawn_timer = spawn_timer + dt
 	if spawn_timer >= spawn_interval then
 		spawn_timer = 0
+		if before_spawn then
+			before_spawn = false
+		end
 		spawnNumber()
 	end
 
 	for _, number in pairs(numbers) do
-		if number.name == tostring(current_binary) then
+		number.y = number.y + (speed * dt)
+		if number.number == current_binary then
 			pancake.trash(pancake.objects, number.ID, "ID")
 			pancake.trash(numbers, number.ID, "ID")
 			score = score + 1
-			spawn_interval = math.max(0.4, 5 - score * 0.02)
-			pancake.physics.gravityY = math.max(12 * pancake.meter, pancake.physics.gravityY + score * 0.02)
-		elseif number.y >= pancake.window.y + 48 * pancake.window.pixelSize then
+			spawn_interval = math.max(4, base_spawn_interval - (score * 0.2))
+			speed = base_speed + (score * 0.1)
+		elseif number.y >= pancake.window.y + 45 * pancake.window.pixelSize then
 			game_over = true
 		end
 	end
 end
 
 function love.mousepressed(x,y,button)
-	pancake.mousepressed(x,y,button) -- Passing your presses to pancake!
+	pancake.mousepressed(x,y,button)
 	x_clicked = x
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+        love.event.quit()
+        return
+    end
+   	pancake.keypressed(key)
 end
